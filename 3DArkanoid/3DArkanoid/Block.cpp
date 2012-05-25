ca52db9fe831160b6d4187329c66f97f0ae60478
bemkuivenhoven@gmail.com <bemkuivenhoven@gmail.com@10c230a6-650f-162a-f2a8-19723b92958e>
2012-05-25 12:23:18 +0000

    model: Added base code for model
    Signed-off-by: Bart Kuivenhoven <bemkuivenhoven@gmail.com>

---
+++ 3DArkanoid/3DArkanoid/Block.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Block.hpp"
+
+Block::Block()
+{
+}
+
+Block::~Block()
+{
+}
+
+void Block::update()
+{
+}
+
+void Block::move(double vect[3])
+{
+}
+
+void Block::use()
+{
+}
+
+void Block::draw()
+{
+}
